48be8275e27a7f258421280d294cfb1022d5ed22
soyandrestrujillo <andymania.1301@gmail.com>
2024-12-04 21:39:46 -0500

    feat: fix load envs

--- src/main/java/com/actions/demo/DemoApplication.java
+++ src/main/java/com/actions/demo/DemoApplication.java
@@ -8,18 +8,21 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
+    try {
+      Dotenv dotenv = Dotenv.load();
+      dotenv.entries().forEach(entry -> {
+        System.setProperty(entry.getKey(), entry.getValue());
+      });
 
-    String ACTOR = dotenv.get("ACTOR");
-    System.out.println(ACTOR + " is learning GitHub Actions");
+      String ACTOR = dotenv.get("ACTOR");
+      System.out.println(ACTOR + " is learning GitHub Actions");
 
-    String LANGUAGE = dotenv.get("LANGUAGE");
-    System.out.println(LANGUAGE + " is my favorite language");
+      String LANGUAGE = dotenv.get("LANGUAGE");
+      System.out.println(LANGUAGE + " is my favorite language");
 
-		SpringApplication.run(DemoApplication.class, args);
+      SpringApplication.run(DemoApplication.class, args);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
 	}
-
 }
